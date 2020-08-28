754a60c8dc7f05745cb148c328b0c05e85c7bfa9
The-Code-Monkey <andy.wilson1234@gmail.com>
2020-08-28 17:28:02 +0100

    feat: add two empty methods

--- src/main/java/mininggadgets/client/MiningGadgetsClient.java
+++ src/main/java/mininggadgets/client/MiningGadgetsClient.java
@@ -3,11 +3,22 @@ package mininggadgets.client;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
+import net.minecraft.util.registry.Registry;
 
 @Environment(EnvType.CLIENT)
 public class MiningGadgetsClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
+        registerRenderers();
+        registerContainerScreens();
         OurKeys.register();
+    }
+
+    private static void registerContainerScreens() {
+
+    }
+
+    private static void registerRenderers() {
+
     }
 }
