d94f088295a319110b9344b0b32abfee999e2af1
The Code Monkey <andy.wilson1234@gmail.com>
2021-12-07 09:48:31 +0000

    fix: stops crashing (needs textures and gui sorting

--- src/main/java/mininggadgets/blockentities/ModificationTableBlockEntity.java
+++ src/main/java/mininggadgets/blockentities/ModificationTableBlockEntity.java
@@ -1,18 +1,40 @@
 package mininggadgets.blockentities;
 
+import mininggadgets.init.MGBlockEntities;
 import net.minecraft.block.BlockState;
-import net.minecraft.block.entity.BlockEntity;
-import net.minecraft.block.entity.BlockEntityType;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.network.packet.s2c.play.BlockEntityUpdateS2CPacket;
 import net.minecraft.util.math.BlockPos;
+import reborncore.api.blockentity.InventoryProvider;
+import reborncore.client.screen.BuiltScreenHandlerProvider;
+import reborncore.client.screen.builder.BuiltScreenHandler;
+import reborncore.client.screen.builder.ScreenHandlerBuilder;
+import reborncore.common.blockentity.MachineBaseBlockEntity;
+import reborncore.common.util.RebornInventory;
 
+public class ModificationTableBlockEntity extends MachineBaseBlockEntity implements InventoryProvider, BuiltScreenHandlerProvider {
+    private final int inventorySize = 2;
 
-import static mininggadgets.init.MGContent.MODIFICATIONTABLE_ENTITY;
+    public RebornInventory<ModificationTableBlockEntity> inventory = new RebornInventory<>(inventorySize, "ModificationTableBlockEntity", 64, this);
 
-public class ModificationTableBlockEntity extends BlockEntity {
+    public enum Slots {
+        TOOL(0),
+        UPGRADE(1);
+
+        private final int id;
+
+        Slots(int number) {
+            id = number;
+        }
+
+        public int getId() {
+            return id;
+        }
+    }
+
     public ModificationTableBlockEntity(BlockPos pos, BlockState state) {
-        super(MODIFICATIONTABLE_ENTITY, pos, state);
+        super(MGBlockEntities.MODIFICATION_TABLE, pos, state);
     }
 
     @Override
@@ -40,5 +62,27 @@ public class ModificationTableBlockEntity extends BlockEntity {
         return super.writeNbt(nbt);
     }
 
+    @Override
+    public RebornInventory<ModificationTableBlockEntity> getInventory() {
+        return inventory;
+    }
 
+    @Override
+    public boolean canBeUpgraded() {
+        return false;
+    }
+
+    @Override
+    public boolean hasSlotConfig() {
+        return false;
+    }
+
+    @Override
+    public BuiltScreenHandler createScreenHandler(int syncID, PlayerEntity player) {
+        return new ScreenHandlerBuilder("modification_table")
+                .player(player.getInventory()).inventory().hotbar().addInventory()
+                .blockEntity(this)
+                .slot(0, 65, 43)
+                .addInventory().create(this, syncID);
+    }
 }
---
+++ src/main/java/mininggadgets/client/gui/GuiModificationTable.java
@@ -0,0 +1,34 @@
+package mininggadgets.client.gui;
+
+import com.mojang.blaze3d.systems.RenderSystem;
+import mininggadgets.MiningGadgets;
+import mininggadgets.blockentities.ModificationTableBlockEntity;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.Identifier;
+import reborncore.client.gui.builder.GuiBase;
+import reborncore.client.screen.builder.BuiltScreenHandler;
+
+public class GuiModificationTable extends GuiBase<BuiltScreenHandler> {
+    private static final Identifier background = new Identifier(MiningGadgets.MOD_ID, "textures/gui/modification_table.png");
+
+    ModificationTableBlockEntity blockEntity;
+
+    public GuiModificationTable(int syncId, final PlayerEntity player, final ModificationTableBlockEntity blockEntity) {
+        super(player, blockEntity, blockEntity.createScreenHandler(syncId, player));
+        this.blockEntity = blockEntity;
+    }
+
+    @Override
+    protected void drawBackground(MatrixStack matrixStack, float lastFrameDuration, int mouseX, int mouseY) {
+        super.drawBackground(matrixStack, lastFrameDuration, mouseX, mouseY);
+
+        RenderSystem.setShaderTexture(0, background);
+        drawTexture(matrixStack, getGuiLeft(), getGuiTop(), 0, 0, this.backgroundWidth, 80);
+    }
+
+    @Override
+    protected void drawForeground(MatrixStack matrixStack, int mouseX, int mouseY) {
+        super.drawForeground(matrixStack, mouseX, mouseY);
+    }
+}
